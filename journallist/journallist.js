fe5ccd7b0c02cf2fde876ecc6c9555c7097f20d9
Tu Nguyen <tunguyen211098@gmail.com>
2022-08-13 02:28:31 +0700

    add login and some responsive

--- journallist/journallist.js
+++ journallist/journallist.js
@@ -1,9 +1,9 @@
-const nav = document.querySelector('.navbar')
-fetch("header.html")
-.then(res => res.text())
-.then(data => {
-    nav.innerHTML = data
-})
+// const nav = document.querySelector('.navbar')
+// fetch("header.html")
+// .then(res => res.text())
+// .then(data => {
+//     nav.innerHTML = data
+// })
 
 const foot = document.querySelector('.footer')
 fetch("footer.html")
@@ -40,3 +40,122 @@ $(document).ready(() => {
 
     document.getElementById('list-bookmarked').innerHTML = listBookmarkedHTML;
 })
+
+
+//login
+function openFormLogin() {
+    document.getElementById("myForm").style.display = "block";
+    document.getElementById("myForm-register").style.display = "none";
+}
+
+function closeFormLogin() {
+    document.getElementById("myForm").style.display = "none";
+}
+function openFormRegister() {
+    document.getElementById("myForm-register").style.display = "block";
+    document.getElementById("myForm").style.display = "none";
+}
+
+function closeFormRegister() {
+    document.getElementById("myForm-register").style.display = "none";
+}
+function switchToRegister() {
+    document.getElementById("myForm").style.display = "none";
+    document.getElementById("myForm-register").style.display = "block";
+}
+function switchToLogin() {
+    document.getElementById("myForm").style.display = "block";
+    document.getElementById("myForm-register").style.display = "none";
+}
+let listUser = [
+    {
+        email: 'abcxyz@gmail.com',
+        password: '123456',
+    },
+    {
+        email: 'test@gmail.com',
+        password: '123456',
+    },
+    {
+        email: 'test1@gmail.com',
+        password: '123456',
+    },
+    {
+        email: 'test2@gmail.com',
+        password: '123456',
+    },
+];
+let login = document.getElementById('ln');
+let register=document.getElementById('reg');
+let tung = window.localStorage.setItem('user', JSON.stringify(listUser));
+let anhien = JSON.parse(window.localStorage.getItem('user'));
+console.log(anhien)
+
+let bigform=document.getElementById('navbar-login-form');
+console.log(document.getElementById("register_submit"));
+console.log(document.querySelector('#login_submit'));
+console.log(document.getElementById('login_submit'));
+document.getElementById('login_submit').onclick = function () {
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+
+    if (email.length < 10 || email.includes('#')) {
+        alert('email chưa đúng định dạng');
+        return;
+    }
+    if (password.length < 6) {
+        alert('password chưa hợp lệ');
+        return;
+    }
+
+
+    for (let user of listUser) {
+        if (user.email === email) {
+            if (user.password === password) {
+                alert('Đăng nhập thành công');
+                window.location = './homepage.html';
+            } else {
+                alert('Password sai');
+            }
+
+            checkEmail = true;
+        }
+    }
+    if (checkEmail === false) {
+        alert('Email chưa được đăng ký');
+    }
+};
+
+
+document.getElementById('register_submit').onclick = function () {
+    const email = document.getElementById('email+').value;
+    const password = document.getElementById('psw+').value;
+
+    if (email.length < 10 || email.includes('#')) {
+        alert('email chưa đúng định dạng');
+        return;
+    }
+    if (password.length < 6) {
+        alert('password chưa đủ điều kiện');
+        return;
+    }
+    let checkEmail = false;
+    for (let user of anhien) {
+        {
+            if (user.email === email) {
+                alert('trùng mail');
+            } else {
+
+                alert('Đăng kí thành công');
+                login.style.display ="none";
+                register.style.display="none";
+                closeFormRegister();
+                return
+            }
+            checkEmail = true;
+        }
+    }
+    if (checkEmail === false) {
+        alert('Email đã được đăng ký');}
+}
+
--- landing-page.js
+++ landing-page.js
@@ -9,7 +9,7 @@ var btn = document.getElementById("login-btn");
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 // When the user clicks the button, open the modal 
-btn.onclick = function() {
+btn.onclick = () => {
   modal.style.display = "block";
 }
 // When the user clicks on <span> (x), close the modal
